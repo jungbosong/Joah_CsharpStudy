a5a464fc17546f0a8015175698133722e7dd6bf2
bosomi <she79560@naver.com>
2023-08-17 14:41:58 +0900

    [ADD] C# 문법 종합반 3주차 강의 과제1 snake게임 제작 (-지연)

---
+++ Jiyeon/ConsoleAppJiyeon/Snake/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace Snake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point snakePosition = new Point(4, 5, '*');
+            Snake snake = new Snake(snakePosition, 1, Direction.RIGHT);
+            
+            Random random = new Random();
+            FoodCreator foodCreator = new FoodCreator(random.Next(1,11), random.Next(1, 11), '$');
+            Point food = foodCreator.CreateFood();
+
+            ConsoleKeyInfo input;
+
+            while (true)
+            {
+                Console.Clear();
+                snake.Draw();
+                food.Draw();
+
+                Console.SetCursorPosition(0, 10);
+                Console.ResetColor();
+                Console.WriteLine("\n방향키를 눌러 뱀을 이동시키고 음식을 먹어 뱀의 몸을 늘리세요.\n뱀: *\t음식: $\n");
+                input = Console.ReadKey(true);
+                switch (input.Key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        snake.SetDirection(Direction.LEFT);
+                        break;
+                    case ConsoleKey.RightArrow:
+                        snake.SetDirection(Direction.RIGHT);
+                        break;
+                    case ConsoleKey.UpArrow:
+                        snake.SetDirection(Direction.UP);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        snake.SetDirection(Direction.DOWN);
+                        break;
+                    default:
+                        break;
+                }
+
+                snake.MoveHeadPoint();
+                if(snake.EatFood(food))
+                {
+                    foodCreator = new FoodCreator(random.Next(1, 11), random.Next(1, 11), '$');
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                snake.Draw();
+
+                Thread.Sleep(100);
+                Console.SetCursorPosition(0,15);
+                Console.WriteLine($"\n현재 길이: {snake.GetLength()}\n먹은 음식의 수: {snake.GetEatCount()}");
+                Thread.Sleep(300);
+            }
+        }
+    }
+
+    public enum Direction
+    {
+        LEFT,
+        RIGHT,
+        UP,
+        DOWN
+    }
+}
---
+++ Jiyeon/ConsoleAppJiyeon/Snake/Snake.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public class Snake
+    {
+        string state;
+        int length;
+        int eatCount = 0;
+        Point headPoint;
+        Direction direction;
+        public List<Point> points = new List<Point>();
+
+        public Snake(Point point, int length, Direction direction)
+        {
+            this.headPoint = point;
+            this.length = length;
+            this.direction = direction;
+            points.Add(headPoint);
+        }
+
+        public void Draw()
+        {
+            for(int i = length-1; i >= 0; i--)
+            {
+                if(i == length-1)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                else 
+                {
+                    Console.ResetColor();
+                }
+                points[i].Draw();
+            }
+        }
+        
+        public void AddBody()
+        {
+            length++;
+        }
+
+        public void LoseLength()
+        {
+            --length;
+            if(length > 1)
+            {
+                points.RemoveAt(0);
+            }
+            if(length <= 0)
+            {
+                length = 1;
+            }
+        }
+
+        public void SetHeadPoint(int x, int y)
+        {
+            headPoint.x = x;
+            headPoint.y = y;
+        }
+
+        public void SetDirection(Direction direction)
+        {
+            this.direction=direction;
+        }
+
+        public int GetLength()
+        {
+            return length;
+        }
+
+        public int GetEatCount()
+        {
+            return eatCount;
+        }
+
+        public void MoveHeadPoint()
+        {
+            switch(direction)
+            {
+                case Direction.LEFT:
+                {
+                    --headPoint.x;
+                    if (headPoint.x <= 0)
+                    {
+                        headPoint.x = 1;
+                        LoseLength();
+                    }
+                    break;
+                }
+                case Direction.RIGHT:
+                {
+                    ++headPoint.x;
+                    if (headPoint.x > 10)
+                    {
+                        headPoint.x = 10;
+                        LoseLength();
+                    }
+                    break;
+                }
+                case Direction.UP:
+                {
+                    --headPoint.y;
+                    if (headPoint.y <= 0)
+                    {
+                        headPoint.y = 1;
+                        LoseLength();
+                    }
+                    break;
+                }
+                case Direction.DOWN:
+                {
+                    ++headPoint.y;
+                    if (headPoint.y > 10)
+                    {
+                        headPoint.y = 10;
+                        LoseLength();
+                    }
+                    break;
+                }
+            }
+            Point newPoint = new Point(headPoint.x, headPoint.y, headPoint.sym);
+            points.Add(newPoint);
+            if(points.Count() > length+1)
+            {
+                points.RemoveAt(0);
+            }
+        }
+
+        public bool EatFood(Point food)
+        {
+            if(headPoint.IsHit(food))
+            {
+                AddBody();
+                eatCount++;
+                return true;
+            }
+            return false;
+        }
+    }
+}
