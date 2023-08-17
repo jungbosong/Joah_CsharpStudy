ede1d2b12304d3a718b23e0b241d57008aa82e73
DoroNyong <pride5623@naver.com>
2023-08-17 14:30:36 +0900

    [ADD] 현교 공부 파일 폴더 생성 (-현교)
    This reverts commit d3b5846ad1c675bbf20b51ed4172818bd4665efb.

--- Hyeongyo/ConsoleAppHyeongyo/ConsoleAppHyeongyo/Program.cs
+++ Hyeongyo/ConsoleAppHyeongyo/ConsoleAppHyeongyo/Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace MyApp
+{
+    // 부모 클래스
+    public class Animal
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public void Eat()
+        {
+            Console.WriteLine("Animal is eating.");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine("Animal is sleeping.");
+        }
+    }
+
+    // 자식 클래스
+    public class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("Dog is bark.");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public void Meow()
+        {
+            Console.WriteLine("Cat is meow.");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine("Cat Sleep!");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+
+            dog.Name = "Bobby";
+            dog.Age = 3;
+
+            dog.Eat();
+            dog.Sleep();
+            dog.Bark();
+
+            Cat cat = new Cat();
+            cat.Name = "Kkami";
+            cat.Age = 10;
+
+            cat.Eat();
+            cat.Sleep();
+            cat.Meow();
+        }
+    }
+}
