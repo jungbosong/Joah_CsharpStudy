522030919b8643329001c4760bbe5e2e125c9ab8
bosomi <she79560@naver.com>
2023-08-17 19:50:33 +0900

    [ADD] C# 문법 종합반 3주차 강의 과제2 블랙잭 게임 제작 (-지연)

---
+++ Jiyeon/ConsoleAppJiyeon/Blackjack/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Blackjack
+{
+    public enum Suit { Hearts, Diamonds, Clubs, Spades }
+    public enum Rank { Two = 2, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 블랙잭 게임을 실행하세요
+            Blackjack blackjack = Blackjack.Instance();
+            Deck deck = Deck.Instance();
+            Dealer dealer = Dealer.Instance();
+            Player player = new Player();
+
+            Console.WriteLine(blackjack.PlayBlackjack(player));
+        }
+    }
+}
