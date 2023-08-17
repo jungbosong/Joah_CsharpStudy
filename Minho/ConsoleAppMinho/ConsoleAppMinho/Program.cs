b3fb2f75c6139ed64dd96df34fb7052d3f770c09
M1NO001 <130371068+M1NO001@users.noreply.github.com>
2023-08-17 10:48:43 +0900

    [ADD] 민호 공부 폴더 파일 생성 (-민호)

--- Minho/ConsoleAppMinho/ConsoleAppMinho/Program.cs
+++ Minho/ConsoleAppMinho/ConsoleAppMinho/Program.cs
@@ -1,1 +1,10 @@
-
+﻿namespace ConsoleAppMinho
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
